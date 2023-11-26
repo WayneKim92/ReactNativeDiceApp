2f6ee9113c84571ff0b56d9971115d2f6e6964ab
WayneKim92 <eunchan0817@gamil.com>
2023-11-26 14:59:08 +0900

    chore: display app name 변경

--- ios/reactnativediceTests/reactnativediceTests.m
+++ ios/reactnativediceTests/reactnativediceTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeHelloBabylonTests : XCTestCase
+@interface reactnativediceTests : XCTestCase
 
 @end
 
-@implementation ReactNativeHelloBabylonTests
+@implementation reactnativediceTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
